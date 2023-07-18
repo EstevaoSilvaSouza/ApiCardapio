f7f80b42ade8e64eb61969fd20601e2269fcbdc6
Estevao <estevaorssouza@hotmail.com>
2023-07-17 23:15:36 -0300

    ok

--- build/index.js
+++ build/index.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app/app"));
 const StartApplication = () => {
-    new app_1.default().app.listen(3080, () => {
+    new app_1.default().app.listen(process.env.PORT || 9001, () => {
+
         console.log(`
         Servidor Rodando!!
 
