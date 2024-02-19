6529c508cafd6f377f9a316aa09b1331db99e821
Estevao Rodrigues Da Silva Souza <estevao.souza@grupocard.com.br>
2024-02-19 16:19:06 -0300

    fix create newUser and Store by User

--- build/service/user/createService.js
+++ build/service/user/createService.js
@@ -5,18 +5,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports._CreateUserService = void 0;
 const genericData_1 = __importDefault(require("../../data/genericData"));
+const userStore_1 = require("../../data/userStore");
 const userRepository_1 = __importDefault(require("../../repository/user/userRepository"));
+const createService_1 = require("../store/createService");
+const findService_1 = require("../store/findService");
 class CreateUserService {
     constructor(e) {
         this.e = e;
         this.handleExecute = async (t) => {
             const ObjCreate = new genericData_1.default(t).returnData();
+            //validar se existe a loja com mesmo nome.
+            const checkStore = await findService_1._FindService.Execute('one', ObjCreate.NameStore, 0, 0, 0);
+            if (checkStore) {
+                throw { message: 'Falha ao cadastrar, Loja já existente.', error: 'l-2001' };
+            }
+            ObjCreate.Type = 'Normal';
+            ObjCreate.Status = true;
+            ObjCreate.IsActive = true;
             const newUser = await this.e.create(ObjCreate);
-            newUser.Type = 'Normal';
-            newUser.Status = true;
-            newUser.IsActive = true;
             if (!newUser) {
                 throw { message: 'Falha ao cadastrar usuário', error: newUser };
+            }
+            const newStore = await createService_1._CreateService.handleExecute({ Description: 'Loja Nova', ImageUrl: '', Name: ObjCreate.NameStore, Type: '', IdUser: newUser.Id });
+            if (newStore) {
+                const InsertStoreuSER = await userStore_1.UsuarioStore.create({ Id_Store: newStore.Id, Id_Usuario: newUser.Id });
+                if (!InsertStoreuSER) {
+                    throw ({ message: 'Falha no processo, favor contato o dev do sistema.', error: 'L-2111' });
+                }
             }
             return newUser;
         };
--- src/service/user/createService.ts
+++ src/service/user/createService.ts
@@ -1,7 +1,11 @@
 import GenericData from "../../data/genericData";
+import { IStore } from '../../data/store';
 import { IUser } from "../../data/user";
+import { UsuarioStore } from "../../data/userStore";
 import { IUserRepository } from "../../repository/user/IuserRepository";
 import UserRepository from "../../repository/user/userRepository";
+import { _CreateService } from "../store/createService";
+import { _FindService } from "../store/findService";
 
 class CreateUserService {
     
@@ -9,13 +13,27 @@ class CreateUserService {
 
     handleExecute = async (t: IUser): Promise<IUser | null> => {
         const ObjCreate = new GenericData<IUser>(t).returnData();
+
+        //validar se existe a loja com mesmo nome.
+        const checkStore = await _FindService.Execute('one',ObjCreate.NameStore,0,0,0) as IStore
+        if(checkStore){
+            throw { message:'Falha ao cadastrar, Loja já existente.',error:'l-2001'}
+        }
+        ObjCreate!.Type = 'Normal';
+        ObjCreate!.Status = true;
+        ObjCreate!.IsActive = true;
         const newUser = await this.e.create(ObjCreate);
-        newUser!.Type = 'Normal';
-        newUser!.Status = true;
-        newUser!.IsActive = true;
 
         if (!newUser) {
             throw { message: 'Falha ao cadastrar usuário', error: newUser };
+        }
+
+        const newStore = await _CreateService.handleExecute({Description:'Loja Nova', ImageUrl:'', Name:ObjCreate.NameStore!,Type:'',IdUser:newUser.Id});
+        if(newStore){
+            const InsertStoreuSER = await UsuarioStore.create({Id_Store:newStore.Id!, Id_Usuario:newUser.Id!});
+                if(!InsertStoreuSER){
+                    throw ({message:'Falha no processo, favor contato o dev do sistema.',error:'L-2111'})
+                }
         }
     
         return newUser;
